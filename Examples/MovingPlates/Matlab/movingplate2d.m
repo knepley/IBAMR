8a2a4dfc08b858afb5181b62c49c250534b5f60e
Amneet Bhalla <mai...@gmail.com>
2011-12-21 00:20:08 +0000

    Adding moving plates example

---
+++ Examples/MovingPlates/Matlab/movingplate2d.m
@@ -0,0 +1,98 @@
+%% Filename :movingplate2d.m
+% Written by : amneet bhalla on taylor@mech.northwestern.edu
+clear all;
+clc;
+%% Mesh parameters.
+Lx = 32.0; Ly = 22.0;
+Nx = 64*4*4; Ny = 64*4*4;
+dx = Lx/Nx; dy = Ly/Ny;
+
+%%plate parameters.
+Length_plate = 1.0;
+num_pts_y = ceil(Length_plate/dy);
+
+%% write out the points. struct_0
+idx = 0;
+    
+    for j = 1:num_pts_y
+        y = (j-0.5)*dy;
+       
+        idx = idx+1;
+        X_array(idx) = 0.0; Y_array(idx) = y;
+    end
+
+XCOM = sum(X_array)/length(X_array);
+%YCOM = sum(Y_array)/length(Y_array);
+
+X_array = X_array - XCOM;
+%Y_array = Y_array - YCOM;
+%plot the plate
+plot(X_array, Y_array, '.'); hold on;
+%axis 'equal'
+
+%write the coordinates in the file
+fid = fopen('../movingplate2d0.vertex','wt');
+fprintf(fid,'%d\n', length(X_array));
+
+for i = 1:length(X_array)
+    fprintf(fid,'%f\t%f\n',X_array(i),Y_array(i));
+end
+fclose (fid);
+
+%% write out the points. struct_1
+idx = 0;
+    
+    for j = 1:num_pts_y
+        y = (j-0.5)*dy + 2 ;
+       
+        idx = idx+1;
+        X_array(idx) = 0.0; Y_array(idx) = y;
+    end
+
+XCOM = sum(X_array)/length(X_array);
+%YCOM = sum(Y_array)/length(Y_array);
+
+X_array = X_array - XCOM;
+%Y_array = Y_array - YCOM;
+ %plot the plate
+plot(X_array, Y_array, '.'); hold on;
+%axis 'equal'
+
+%write the coordinates in the file
+fid = fopen('../movingplate2d1.vertex','wt');
+fprintf(fid,'%d\n', length(X_array));
+
+for i = 1:length(X_array)
+    fprintf(fid,'%f\t%f\n',X_array(i),Y_array(i));
+end
+fclose(fid);
+
+%%  write out the points. struct_2
+idx = 0;
+    
+    for j = 1:num_pts_y
+        y = (j-0.5)*dy - 2 ;
+       
+        idx = idx+1;
+        X_array(idx) = 0.0; Y_array(idx) = y;
+    end
+
+XCOM = sum(X_array)/length(X_array);
+%YCOM = sum(Y_array)/length(Y_array);
+
+X_array = X_array - XCOM;
+%Y_array = Y_array - YCOM;
+ %plot the plate
+plot(X_array, Y_array, '.'); hold off;
+%axis 'equal'
+
+%write the coordinates in the file
+fid = fopen('../movingplate2d2.vertex','wt');
+fprintf(fid,'%d\n', length(X_array));
+
+for i = 1:length(X_array)
+    fprintf(fid,'%f\t%f\n',X_array(i),Y_array(i));
+end
+fclose(fid);
+
+
---
+++ Examples/MovingPlates/MovingPlateKinematics.h
@@ -0,0 +1,122 @@
+// Filename: MovingPlateKinematics.h
+// Written by amneet bhalla on meghnad@mech.northwestern.edu
+// Created on 12/20/2011.
+
+// This is a concrete class which provides kinematics of Moving plate to ConstraintIBMethod class.
+     
+ 
+#ifndef included_MovingPlateKinematics
+#define included_MovingPlateKinematics
+
+///////////////////////////////////////// INCLUDES //////////////////////////////////////////
+
+//IBAMR INCLUDES
+#include "../../ConstraintIBKinematics.h"
+
+
+//C++ INCLUDES
+#include <vector>
+
+namespace IBAMR
+{
+  
+  /*!
+   * \brief Class MovingPlateKinematics provides definition for base ConstraintIBKinematics class.
+   */
+class MovingPlateKinematics 
+    : public ConstraintIBKinematics
+{
+  
+  
+public:
+    
+    /*!
+     * \brief Constructor.
+     */
+    MovingPlateKinematics(
+        const std::string& object_name,
+        SAMRAI::tbox::Pointer<SAMRAI::tbox::Database> input_db,
+	IBTK::LDataManager* l_data_manager,
+        bool register_for_restart = true);
+    
+    /*!
+     * Destructor.
+     */
+    virtual 
+    ~MovingPlateKinematics();
+    
+    /*!
+     * Set kinematics velocity at new time for Moving plate.
+     */
+    virtual void
+    setNewKinematicsVelocity(
+        const double Time,
+        const std::vector<double>& incremented_angle_from_reference_axis,
+        const std::vector<double>& center_of_mass,
+        const std::vector<double>& tagged_pt_position);
+    
+    /*!
+     * Get the kinematics velocity at new time for Moving plate on the specified level.
+     * 
+     */
+    virtual const std::vector<std::vector<double> >&
+    getNewKinematicsVelocity(const int level) const;
+    
+    /*!
+     * Get the kinematics velocity at current time for Moving plate on the specified level.
+     * 
+     */
+    virtual const std::vector<std::vector<double> >&
+    getCurrentKinematicsVelocity(const int level) const;
+    
+  
+    /*!
+     * Set the shape of Moving plate at new time for the structure on all levels.
+     */
+    virtual void
+    setNewShape();
+    
+    /*!
+     * Get the shape of structure at new time  on the specified level.
+     */
+    virtual const std::vector<std::vector<double> >&
+    getNewShape(const int level) const;
+    
+
+private:
+  
+    /*!
+     * Deleted default ctor.
+     */
+    MovingPlateKinematics();
+    
+    /*!
+     * Deleted default copy ctor.
+     */
+    MovingPlateKinematics(
+        const MovingPlateKinematics& from);
+  
+    /*!
+     * Deleted default assignment.
+     */
+    MovingPlateKinematics&
+    operator = (
+        const MovingPlateKinematics& that);
+  
+    
+    /*!
+     * New kinematics velocity. New shape of the body.
+     * 
+     * \NOTE Current velocity is always equal to new velocity. Position is 
+     * updated via CONSTRAINT_VELOCITY method, so new shape is not filled in.
+     */
+    std::vector<std::vector<double> >  d_new_kinematics_vel, d_new_shape;
+  
+  
+  
+}; // MovingPlateKinematics 
+  
+ 
+} //IBAMR
+
+#endif //included_MovingPlateKinematics
